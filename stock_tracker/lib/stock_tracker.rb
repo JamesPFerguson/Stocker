89899fcdfe49e11d175bd6be048b2cba89e6a767
James Ferguson <ncjamesferguson@gmail.com>
2018-01-06 21:14:31 +0000

    commented in link

--- stock_tracker/lib/stock_tracker.rb
+++ stock_tracker/lib/stock_tracker.rb
@@ -3,3 +3,5 @@ require "stock_tracker/version"
 module StockTracker
   # Your code goes here...
 end
+
+# https://finviz.com/screener.ashx?v=111&f=fa_pe_low,fa_pfcf_low,fa_ps_u3,geo_usa,ta_perf_13wup,ta_perf2_4wup&ft=4&o=-perf26w
