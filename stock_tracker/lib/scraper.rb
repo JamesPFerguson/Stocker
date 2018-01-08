c800361e09e7234aec2426df7210ee85341f69af
James Ferguson <ncjamesferguson@gmail.com>
2018-01-08 00:34:10 +0000

    figuring out what a stock has and how to scrape it

--- stock_tracker/lib/scraper.rb
+++ stock_tracker/lib/scraper.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'pry'
 require 'open-uri'
+require_relative './stock.rb'
 
 class Scraper
 
@@ -8,8 +9,9 @@ class Scraper
   def self.scrape_from_main
     stocks = Nokogiri::HTML(open("https://finviz.com/screener.ashx?v=111&f=fa_pe_low,fa_pfcf_low,fa_ps_u3,geo_usa,ta_perf_13wup,ta_perf2_4wup&ft=4&o=-perf26w"))
 
-    stocks.css(".table-dark-row-cp").each do |column|
-      column
+    stocks.css(".table-dark-row-cp").each do |columns|
+      columns.css(".screener-body-table-nw").each do |column|
+
       binding.pry
     end
 
---
+++ stock_tracker/lib/stock.rb
@@ -0,0 +1,14 @@
+class Stock
+
+  @@stocks
+
+attr_accessor :ticker, :name, :price, :volume, :market_cap, :industry, :sector, :number, :pe_ratio, :week_perf, :month_perf, :quart_perf, :half_perf, :year_perf
+
+  def initialize
+  end
+
+  def self.stocks
+    @@stocks
+  end
+
+end
