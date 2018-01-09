d62ceab35f9a158119ef374317ff828fce6e54e0
James Ferguson <ncjamesferguson@gmail.com>
2018-01-09 01:31:18 +0000

    added environment

---
+++ stock_tracker/config/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/stock_tracker/cli.rb'
+require_relative '../lib/stock_tracker/scraper.rb'
+require_relative '../lib/stock_tracker/stock.rb'
