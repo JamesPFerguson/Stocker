4a9685bc6afa9197ddf10f2aa4d4b9c900bf9fdb
James Ferguson <ncjamesferguson@gmail.com>
2018-01-12 23:49:25 +0000

    fixed bugs in CLI

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-require "stock_tracker/version"
+require_relative '../lib/stock_tracker/version.rb'
 require_relative '../lib/stock_tracker/cli.rb'
 require_relative '../lib/stock_tracker/scraper.rb'
 require_relative '../lib/stock_tracker/stock.rb'
